7847291b10fbca1fc768b757ba7dae910e6ab27b
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2024-07-06 13:17:57 -0300

    Version del proyecto JAVA CRUD APIREST con las modificaciones necesarias para subirlo a RAILWAY

---
+++ src/main/java/com/example/proyectofinal/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.example.proyectofinal.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.proyectofinal.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+}
